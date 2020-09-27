bffad67bee1868b7fc01ab06e0b561cd72f6f615
Nils Jørgen Mittet <njmittet@gmail.com>
2020-09-27 13:43:33 +0200

    Add Rust owenership example

---
+++ rust-ownership/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    copy_in_function();
+    copy_with_function()
+}
+
+fn copy_in_function() {
+    let vec1: Vec<String> = vec![String::from("First"), String::from("Second")];
+    let mut vec2: Vec<String> = Vec::new();
+    vec2.push(vec1.get(1).unwrap().clone());
+}
+
+fn copy_with_function() {
+    let vec1: Vec<String> = vec![String::from("First"), String::from("Second")];
+    let mut vec2: Vec<String> = Vec::new();
+    let vec3 = copy_to_new_vec(&vec1, &mut vec2);
+    vec3.get(0);
+}
+
+// Functions that return a reference to an object must use a lifetime paramter in order
+// to tell the the compiler exactly which object is referenced. If the object that is
+// being referenced goes out of scope while the reference is still in scope, the
+// compiler will complain. Lifetime parameters are normally inferred by the compiler,
+// but in this example, because there are two parameters that the return value could
+// potentially be referencing, the lifetime parameters must be explicitly provided.
+fn copy_to_new_vec<'a>(vec1: &Vec<String>, vec2: &'a mut Vec<String>) -> &'a mut Vec<String> {
+    vec2.push(vec1.get(1).unwrap().to_string());
+    return vec2;
+}
